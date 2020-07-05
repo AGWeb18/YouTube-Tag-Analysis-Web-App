9a750e9dbef16f47c13de207259d3e3d499ffb29
Anthony <aridding18@gmail.com>
2020-07-05 14:01:57 -0400

    MVP-complete

--- YouTube_Tag_Analysis/YouTube_Tag_Analysis/views.py
+++ YouTube_Tag_Analysis/YouTube_Tag_Analysis/views.py
@@ -6,8 +6,17 @@ https://docs.microsoft.com/en-us/visualstudio/liveshare/use/vscode
 from datetime import datetime
 from YouTube_Tag_Analysis import app
 from flask import Flask, render_template, request
-from YouTube_Tag_Analysis.youtube_functions import get_top_50_vids_details
+from YouTube_Tag_Analysis.youtube_functions import tag_finder_main
+      
 
+#TODO: Optimize API call for Quota
+#TODO: Write a function to get user channel id based on text 
+#TODO: Prepare search phrase for API (replace spaces)
+#TODO: Hosting to launch
+#TODO: Re-Design/Format Output -- closer to the PDF that we send
+
+
+@app.route('/home', methods=['GET', 'POST'])
 @app.route('/', methods=['GET', 'POST'])
 def home():
     errors = ''
@@ -20,34 +29,47 @@ def home():
             errors += '<p>{!r} is not a valid search phrase.</p>\n'.format(request.form['search_phrase'])
 
         if search_phrase is not None:
-            result = get_top_50_vids_details(search_phrase)
+            #result = get_top_50_vids_details(search_phrase) 
+            #multi_var_dict = tag_finder_main(search_phrase)      
             
-            return '''
-                <html>
-                    <body>
-                        <h1>The top videos are shown below:</h1>
-						<p>{result}</p>
-                        <p><a href="/">Click here to search again</a>
-                    </body>
-                </html>
-            '''.format(result=result.to_html())
+            #TODO: connect API results to front end
+
+            tags = ['Runescape', 'Runescape 3', 'tet gsg', 'fggih 35', 'sfhs 34', 'dgdghdg sfh 47', 'tet gsg', 'fggih 35', 'sfhs 34', 'dgdghdg sfh 47']
+
+            top_tag_count, top_tag_vid_count = 50, 30
+            competitor_top_tag_count, competitor_top_tag_vid_count = 80, 45
+
+            num_vids = 45
+            competitor_num_vid = 50
+
+            top_tag_score = round((top_tag_count/(num_vids*10))*100, 2)
+            competitor_top_tag_score = round(competitor_top_tag_count/(competitor_num_vid*10)*100,2)
+
+            multi_var_dict = {'top_tag_score':top_tag_score, 'competitor_top_tag_score':competitor_top_tag_score}
+
+            return render_template(
+                'results.html',
+                title='Home',
+                errors=errors,
+                search_phrase = search_phrase,
+                top_tags = top_tags,
+                top_tag_score = top_tag_score,
+                competitor_top_tag_score = competitor_top_tag_score,
+                top_tag_vid_count = top_tag_vid_count,
+                num_vids = num_vids,
+                competitor_num_vid = competitor_num_vid,
+                data = multi_var_dict
+                #top_tags = multi_var_dict['top_tags']
+            )
+      
     else:
-        return '''
-            <html>
-                <body>
-                    {errors}
-                    <p>Enter your YouTube search phrase:</p>
-                    <form method="post" action=".">
-                        <p><input name="search_phrase" /></p>
-                        <p><input type="submit" value="Search" /></p>
-                    </form>
-                </body>
-            </html>
-        '''.format(errors=errors)
+        return render_template(
+        'index.html',
+        title='Home',
+        errors=errors
+    )
 
-
-
-
+    
 """
 @app.route('/home')
 def home():
--- YouTube_Tag_Analysis/YouTube_Tag_Analysis/youtube_functions.py
+++ YouTube_Tag_Analysis/YouTube_Tag_Analysis/youtube_functions.py
@@ -102,12 +102,152 @@ def get_top_50_vids_details(search_phrase):
 		  full_vid_response = pd.concat([full_vid_response, temp_df], axis=0)
 	  return full_vid_response
 
-def color_negative_red(val):
-	"""
-	Takes a scalar and returns a string with
-	the css property `'color: red'` for negative
-	strings, black otherwise.
-	"""
-	color = 'red' if val < 0 else 'black'
-	return 'color: %s' % color
+
+def get_top_tags(df, num):
+  """get the list of top tags 
+
+  Args:
+      df (DataFrame): DataFrame with videos to get tags from)
+
+  Returns:
+      DataFrame : the top tags of 'num' length
+      
+  """
+  video_tags = values_from_list_of_list(df[df.tags.notnull()].tags)
+
+  top_tags_count = Counter(video_tags)
+  top_tags = [item[0] for item in top_tags_count.most_common(num)]
+  print(f"top_tags: {top_tags}")
+  return top_tags
+
+def get_videos(chan_id):
+  """get the list of videos for channel id (up to 50)
+
+  Args:
+     chan_id, channel ID
+  Returns:
+      video_id (string) : list of video IDs
+  """
+      
+  videos_request = youtube.search().list(
+    part='snippet', 
+    q=search_phrase,
+    channelId= chan_id,
+    order='date',
+    type='video',
+    maxResults=50)
+
+  videos_results = videos_request.execute()
+  
+  with open('videos_results.json', 'w') as outfile:
+    json.dump(videos_results, outfile, indent=2)
+
+  return videos_results
+
+def videos_to_update(vid_df):
+  videos_to_update = []
+
+  #Get a list of videos to update the tags based on
+  for item in vid_df["items"]:
+    #print(f"video title: {item['snippet']['title']}")
+    videos_to_update.append(item['id']['videoId'])
+
+  video_ids = ",".join(videos_to_update)
+
+  return video_ids
+
+def get_old_tags(video_id):
+  """get the list of old tags based on video id list 
+
+  Args:
+      video_id (DataFrame): DataFrame with videos to update
+
+  Returns:
+      vid_tags_df (DataFrame) : df of videos to update tags
+  """
+  
+  videos_details = youtube.videos().list(
+    part='snippet,contentDetails,statistics,topicDetails',
+    id = video_id
+  )
+
+
+  videos_details_results = videos_details.execute()
+
+  with open('videos_details_results.json', 'w') as outfile:
+    json.dump(videos_details_results, outfile, indent=2)
+
+
+  l_of_responses = []
+
+  for item in videos_details_results['items']:
+      try:
+          #print(f"Old Tags: {item['snippet']['tags']}")
+      
+          ID = item['id']
+          title = item['snippet']['title']
+          catID = item['snippet']['categoryId']
+          tags = item['snippet']['tags']
+          l_of_responses.append([ID, title, catID, tags])
+      except:
+          continue
+          #print("No tags")
+
+  vid_tags_df = pd.DataFrame(
+      l_of_responses,
+      columns=[
+          "vidId", "vidTitle","categoryId", "tags"]
+          )
+
+  return vid_tags_df
+
+def get_top_tag_info(tag_list, top_tags):
+  #number of videos with a top tag
+  top_tag_vid_count = 0
+  #number of top tags used in most recent videos
+  top_tag_count = 0
+  
+  tag_list = tag_list.dropna()
+
+  for tags in tag_list:
+
+    if len(tags)>1:
+      
+      lower_tags = [t.lower() for t in tags]
+
+      vid_has_top_10_tag = False
+      for top_tag in top_tags:
+        if top_tag in lower_tags:
+          vid_has_top_10_tag = True
+          top_tag_count = top_tag_count + 1
+      if vid_has_top_10_tag:
+        top_tag_vid_count = top_tag_vid_count + 1
+
+  return (top_tag_count, top_tag_vid_count)
+
+def tag_finder_main(search_phrase):
+	full_vid_response = get_top_50_vids_details(search_phrase)
+	competitor_num_vid = len(full_vid_response.tags)
+	top_tags = get_top_tags(full_vid_response, 10)
+	return {'top_tags':top_tags, }
+
+#	vids = get_videos(chan_id)
+#	vid_ids = videos_to_update(vids)
+#	vid_tags_df = get_old_tags(vid_ids)
+#	num_vids = len(vids["items"])
+#	top_tag_count, top_tag_vid_count = get_top_tag_info(vid_tags_df.tags, top_tags)
+#	competitor_top_tag_count, competitor_top_tag_vid_count = get_top_tag_info(full_vid_response.tags, top_tags)
+#	return {'vids':vids, 'vid_ids':vid_ids, 'vid_tags_df':vid_tags_df, 'num_vids':num_vids, 
+#		    'top_tag_count':top_tag_count, 'top_tag_vid_count':top_tag_vid_count, 
+#			'competitor_top_tag_count':competitor_top_tag_count, 
+#			'competitor_top_tag_vid_count':competitor_top_tag_vid_count}
+	
+#count_of_top_tag = f"There are {top_tag_vid_count}/{num_vids} videos with a top tag."
+#top_tag_score = f"Your top tag score is {(top_tag_count/(num_vids*10))*100}%."
+
+
+#competitor_count f"There are {competitor_top_tag_vid_count}/{competitor_num_vid} videos with a top tag.")
+#print(f"Competitor top tag score is {competitor_top_tag_count/(competitor_num_vid*10)*100}%.")
+
+
 
