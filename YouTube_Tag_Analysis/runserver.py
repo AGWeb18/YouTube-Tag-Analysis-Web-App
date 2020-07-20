afbf627555accd25e991e986faaa6922078d796e
Anthony <aridding18@gmail.com>
2020-07-19 22:06:44 -0400

    created wsgi

--- YouTube_Tag_Analysis/runserver.py
+++ YouTube_Tag_Analysis/runserver.py
@@ -6,14 +6,9 @@ from os import environ
 from YouTube_Tag_Analysis import app
 
 if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(environ.get('SERVER_PORT', '1818'))
-    except ValueError:
-        PORT = 1818
     
-    app.run(host='localhost', port=1818)
-#    app.run(HOST, PORT)
+    app.run()
+
 
 
       
---
+++ wsgi.py
@@ -0,0 +1,8 @@
+
+from os import environ
+from YouTube_Tag_Analysis.YouTube_Tag_Analysis import app
+
+if __name__ == '__main__':
+    
+    app.run()
+
