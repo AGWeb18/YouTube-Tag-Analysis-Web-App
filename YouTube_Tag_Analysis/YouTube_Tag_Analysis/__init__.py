a98256a2dd49707858faa10a8aa8410825b0f810
Anthony <aridding18@gmail.com>
2020-07-19 22:22:26 -0400

    import statement

--- YouTube_Tag_Analysis/YouTube_Tag_Analysis/__init__.py
+++ YouTube_Tag_Analysis/YouTube_Tag_Analysis/__init__.py
@@ -5,4 +5,4 @@ The flask application package.
 from flask import Flask
 app = Flask(__name__)
 app.secret_key = b"85\xf9\x80'\xdd\x95\xf5\x99\xf1\\!i\xed\xd0q"
-import views
+import YouTube_Tag_Analysis.YouTube_Tag_Analysis.views
